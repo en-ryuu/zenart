8f4aa034db86b33453ffd6909cbddef6121574a9
Abhishek Pandey <berzerkxx@gmail.com>
2021-03-20 17:18:30 +0545

    Quality of life changes
    - Added starfiled to index
    - Fixed color scheme for index
    - updated weave.js for better experience
    - small quality of life fixes

--- .js/weave.js
+++ .js/weave.js
@@ -1,34 +1,12 @@
-// test variables
-var j;
 // Select number of cross sections
 var symmetry, sets;
 
-//Define global variables for curve points and anchors, mouseX, mouseY variables 
+//Define global variables for curve points and anchors, mouseX, mouseY variables
 var sx, sy, ax1, ay1, ax2, ay2, ex, ey, mx, my, pmx, pmy, t;
 var gui, noDraw, resetV;
 
-// Color Tones 
-const TONES_1 = [
-  [188, 37, 88, 18],
-  [188, 37, 88, 50],
-  [163, 83, 163, 18],
-  [163, 83, 163, 80]
-]; // Red/Pink
-const TONES_2 = [
-  [27, 160, 152, 18],
-  [27, 160, 152, 30],
-  [25, 255, 178, 30]
-]; // Blue / Aqua
-const TONES_3 = [
-  [205, 1, 222, 18],
-  [205, 1, 222, 30]
-]; // Green?
-
-
-
 // Define the angles for Symmetry
 let angle;
-
 
 var settings = {
   sym: 6,
@@ -36,46 +14,44 @@ var settings = {
   dual: false,
   n: 75,
   stx: 2,
-  auto: true,
-  nFrame: 200,
-  frames: 60
+  auto: false,
+  nFrame: 90,
+  frames: 60,
+  t: 12,
 };
 
 // Color Picker Object
 var ColorObject = function () {
-  this.color = [27, 160, 152]; // RGB with alpha
-  this.color2 = [220, 220, 220];
-  this.a = 50;
-  this.bgColor = 2
+  this.color = [0, 255, 237]; // RGB with alpha
+  // this.color2 = [188, 61, 61];
+  this.color2 = [255, 255, 255];
+  this.a = 75;
+  this.bgColor = 1;
 };
 
 function setBackground() {
-  if(colorObject.bgColor == 1){
-    background('#0b0918');
+  if (colorObject.bgColor == 1) {
+    background("#000000");
   }
-  if(colorObject.bgColor == 2){
-    background('#000000');
+  if (colorObject.bgColor == 2) {
+    background("#0b0918");
   }
-  if(colorObject.bgColor == 3){
-    background('#cccccc');
+  if (colorObject.bgColor == 3) {
+    background("#cccccc");
   }
-  if(colorObject.bgColor == 4){
-    background('#ffffff');
+  if (colorObject.bgColor == 4) {
+    background("#ffffff");
   }
-  if(colorObject.bgColor == 5){
-    background('#0b0918');
+  if (colorObject.bgColor == 5) {
+    background("#0b0918");
   }
 }
 
 var colorObject = new ColorObject();
 
-
 // Setup initial Canvas ------------------------------------------------------------------------------------------------
 
 function setup() {
-
-  //test 
-  j = 0;
   // colorMode(HSB, 300);
 
   // Default Symmetry
@@ -83,10 +59,11 @@ function setup() {
   symmetry = settings.sym;
   angle = 360 / symmetry;
 
-  createGUI();
-  noDraw = false;
+  createGUI(); // Create a GUI object for this sketch
 
-  // frameRate(25); // How many frames per second (default: 20 or 25) 
+  noDraw = false; // Drawing is enabled
+
+  // frameRate(25); // How many frames per second (default: 20 or 25)
 
   stroke(0, 50); // Grayscale(0) , Alpha (18)
   noFill(); // Don't fill the color inside shapes
@@ -94,17 +71,15 @@ function setup() {
   t = 0; // Increase the t value for noise
 
   canvas = createCanvas(window.innerWidth, window.innerHeight);
-  canvas.style('display', 'block');
-  canvas.parent('sketch-holder');
+  canvas.style("display", "block");
+  canvas.parent("sketch-holder");
 
   // background('#0f0c24'); // The sweet easy on eye color
   // background(colorObject.bgColor);
   setBackground();
 }
 
-
-
-// Clear the screen 
+// Clear the screen
 function clearScreen() {
   // background('#0b0918');
   setBackground();
@@ -121,7 +96,6 @@ function screenFull() {
   let fs = fullscreen();
   fullscreen(!fs);
 }
-
 
 // Main function for drawing. Loops infinitely until stopped.
 function draw() {
@@ -140,8 +114,13 @@ function draw() {
   noiseSeed(settings.n); // Define a specific noise seed
 
   // If mouse is inside the canvas
-  if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height && noDraw == false) {
-
+  if (
+    mouseX > 0 &&
+    mouseX < width &&
+    mouseY > 0 &&
+    mouseY < height &&
+    noDraw == false
+  ) {
     //Translate mouse position to match translated canvas
     setMousePos();
 
@@ -155,7 +134,7 @@ function draw() {
       }
     }
   }
-
+  t += settings.t / 1000; // Increment t for varying noise
 }
 
 function setMousePos() {
@@ -165,13 +144,10 @@ function setMousePos() {
   pmy = pmouseY - height / 2;
 }
 
-
-
 function drawFunc() {
-  // Draw one part of the symmetry and copy it to the remaining by rotating 
+  // Draw one part of the symmetry and copy it to the remaining by rotating
   for (let i = 0; i < symmetry; i++) {
-
-    // Color Definitions 
+    // Color Definitions
     // let random_index = Math.floor(Math.random() * TONES_2.length); // For gradient effect
     const [r, g, b] = colorObject.color; // Define primary color
     const a = colorObject.a;
@@ -185,20 +161,37 @@ function drawFunc() {
     assignPoints(); // Define the Start, End and anchor points for the bezier curve
 
     drawBezier(sx, sy, ax1, ay1, ax2, ay2, ex, ey, a); // Function to draw the Bezier curves
-    drawBezier(sx / 2, sy / 2, ax1 / 2, ay1 / 2, ax2 / 2, ay2 / 2, ex / 2, ey / 2,a );
-    drawBezier(sx / 4, sy / 4, ax1 / 4, ay1 / 4, ax2 / 4, ay2 / 4, ex / 4, ey / 4, a);
-
-    t += 0.0009; // Increment t for varying noise
-
+    drawBezier(
+      sx / 2,
+      sy / 2,
+      ax1 / 2,
+      ay1 / 2,
+      ax2 / 2,
+      ay2 / 2,
+      ex / 2,
+      ey / 2,
+      a
+    );
+    drawBezier(
+      sx / 4,
+      sy / 4,
+      ax1 / 4,
+      ay1 / 4,
+      ax2 / 4,
+      ay2 / 4,
+      ex / 4,
+      ey / 4,
+      a
+    );
     //clearFrame(); // For auto clearing canvas after x frames
   }
 }
 
 function drawBezier(sx, sy, ax1, ay1, ax2, ay2, ex, ey, a) {
-  //    Secondary color definitions 
+  //    Secondary color definitions
   const [r2, g2, b2] = colorObject.color2; // Define secondary color
 
-  // Initial Bezier curves 
+  // Initial Bezier curves
   bezier(sx, sy, ax1, ay1, ax2, ay2, ex, ey);
   push();
   scale(1, -1);
@@ -210,7 +203,6 @@ function drawBezier(sx, sy, ax1, ay1, ax2, ay2, ex, ey, a) {
 
   bezier(sx, sy, ax1, ay1, ax2, ay2, ex, ey);
   pop();
-
 }
 
 //Clear Frames after every 'n' frames
@@ -218,75 +210,96 @@ function clearFrame() {
   if (frameCount % settings.nFrame == 0) {
     clearScreen();
   }
-
 }
 
 // Gui for settings
 function createGUI() {
   // gui = new dat.GUI();
   gui = new dat.GUI();
-  gui.domElement.id = 'gui';
+  gui.domElement.id = "gui";
   gui_container.appendChild(gui.domElement);
   var obj = {
     Save: function () {
-      save('design.jpg');
+      save("design.jpg");
     },
     Clear: function () {
-      background('#0f0c24');
+      setBackground();
     },
     Link: function () {
       window.open("https://www.deviantart.com/en-ryuu", _self);
-    }
+    },
   };
-  gui.add(obj, 'Save').name('Save Canvas');
-  gui.add(obj, 'Clear').name('Clear Canvas (R)');
-  gui.add(settings, 'sym').name('n-fold symmetry').min(2).step(1).max(8).onChange(settings.resetVar);
+  gui.add(obj, "Save").name("Save Canvas");
+  gui.add(obj, "Clear").name("Clear Canvas (R)");
+  gui
+    .add(settings, "sym")
+    .name("n-fold symmetry")
+    .min(2)
+    .step(1)
+    .max(8)
+    .onChange(settings.resetVar);
+  gui
+    .add(settings, "t")
+    .name("Speed of variation")
+    .min(1)
+    .step(1)
+    .max(20)
+    .onChange(settings.resetVar);
 
-  var guiColor = gui.addFolder('Color Options');
+  var guiColor = gui.addFolder("Color Options");
   guiColor.open();
-  guiColor.addColor(colorObject, 'color').name('Primary Color');
-  guiColor.add(colorObject, 'a').name('Max opacity').min(5).max(255);
-  guiColor.add(settings, 'dual').name('Dual Color');
-  guiColor.addColor(colorObject, 'color2').name('Secondary Color');
-  guiColor.add(colorObject, 'bgColor', {
+  guiColor.addColor(colorObject, "color").name("Primary Color");
+  guiColor.add(colorObject, "a").name("Max opacity").min(5).max(255);
+  guiColor.add(settings, "dual").name("Dual Color");
+  guiColor.addColor(colorObject, "color2").name("Secondary Color");
+  guiColor.add(colorObject, "bgColor", {
     Darker: 1,
     Dark: 2,
     Lighter: 3,
     Light: 4,
-    Picker: 5
-  }).name('Background').onChange(clearScreen);
+    Picker: 5,
+  }).name("Background").onChange(clearScreen);
 
-  gui.add(settings, 'stx', {
-    Weave: 1,
-    Uniform: 2,
-    Uniform2: 3,
-    Flower: 4,
-    Vortex: 5,
-    Frost: 6,
-    UseAuto: 7
-  }).name('Presets');
+  gui
+    .add(settings, "stx", {
+      Weave: 1,
+      Uniform: 2,
+      Uniform2: 3,
+      Flower: 4,
+      Vortex: 5,
+      Frost: 6,
+      UseAuto: 7,
+    })
+    .name("Presets");
 
-  var guiAutomation = gui.addFolder('Automation');
-  guiAutomation.add(settings, 'auto').name('AutoDraw');
-  guiAutomation.add(settings, 'nFrame').name('Clear After n frame')
+  var guiAutomation = gui.addFolder("Automation");
+  guiAutomation.add(settings, "auto").name("AutoDraw");
+  guiAutomation.add(settings, "nFrame").name("Clear After n frame");
 
-  var guiAdvanced = gui.addFolder('Advanced options');
-  guiAdvanced.add(settings, 'frames').name('Frames/sec').min(22).step(4).max(120).onChange(settings.resetVar);
-  guiAdvanced.add(settings, 'n').name('Noise seed').min(1).max(1000);
+  var guiAdvanced = gui.addFolder("Advanced options");
+  guiAdvanced.add(settings, "frames").name("Frames/sec")
+    .min(22)
+    .step(4)
+    .max(120)
+    .onChange(settings.resetVar);
+  guiAdvanced.add(settings, "n").name("Noise seed").min(1).max(1000);
 
-  var deviantart = gui.add({
-    fun: function () {
-      window.open('https://www.deviantart.com/en-ryuu');
-      ga('send', 'event', 'link button', 'deviantart');
-    }
-  }, 'fun').name('Author Profile');
-  deviantart.__li.className = 'cr function bigFont';
-  deviantart.__li.style.borderLeft = '3px solid #8C8C8C';
-  var deviantartIcon = document.createElement('span');
+  var deviantart = gui
+    .add({
+        fun: function () {
+          window.open("https://www.deviantart.com/en-ryuu");
+          ga("send", "event", "link button", "deviantart");
+        },
+      },
+      "fun"
+    )
+    .name("Author Profile");
+  deviantart.__li.className = "cr function bigFont";
+  deviantart.__li.style.borderLeft = "3px solid #8C8C8C";
+  var deviantartIcon = document.createElement("span");
   deviantart.domElement.parentElement.appendChild(deviantartIcon);
-  deviantartIcon.className = 'icon deviantart';
+  deviantartIcon.className = "icon deviantart";
   // gui.add(settings,'brushStroke').name('StrokeSize').min(2).step(1).max(5).onChange(settings.resetVar);
-
 
   if (isMobile()) {
     gui.close();
@@ -298,7 +311,7 @@ function isMobile() {
   return /Mobi|Android/i.test(navigator.userAgent);
 }
 
-// On mouse over gui, Disable Drawing 
+// On mouse over gui, Disable Drawing
 function setStatus() {
   noDraw = true;
 }
@@ -313,8 +326,7 @@ settings.resetVar = function () {
 
 settings.refresh = function () {
   setBackground();
-}
-
+};
 
 //Preset definitions -------------------------------------------------------------------------------------------------------------
 function assignPoints() {
@@ -332,11 +344,11 @@ function assignPoints() {
 
   if (settings.stx == 2) {
     //Sweet Spot
-    sx = width / 2 * noise(t + 15);
+    sx = (width / 2) * noise(t + 15);
     ax1 = width * noise(t + 25);
     ax2 = width * noise(t + 35);
     ex = width * noise(t + 45);
-    sy = height / 2 * noise(t + 55);
+    sy = (height / 2) * noise(t + 55);
     ay1 = height * noise(t + 65);
     ay2 = height * noise(t + 75);
     ey = height * noise(t + 85);
@@ -388,27 +400,25 @@ function assignPoints() {
     ay1 = pmx * noise(t + 65);
     ay2 = pmx * noise(t + 75);
     ey = pmx * noise(t + 85);
-
   }
 
   if (settings.stx == 7) {
-    sx = width / 2 * noise(t + 15);
+    sx = (width / 2) * noise(t + 15);
     ax1 = width * noise(t + 25);
     ax2 = width * noise(t + 35);
     ex = width * noise(t + 45);
-    sy = height / 2 * noise(t + 55);
+    sy = (height / 2) * noise(t + 55);
     ay1 = height * noise(t + 65);
     ay2 = height * noise(t + 75);
     ey = height * noise(t + 85);
   }
 
-//For Debugging purposes
-// function printPoints() {
-//   print('MouseX: ' + mx + ' MouseY: ' + my);
-//   print('Startx: ' + sx + ' Starty: ' + sy);
-//   print('AnchorX1: ' + ax1 + ' AnchorY1: ' + ay1);
-//   print('AnchorX2: ' + ax2 + ' AnchorY2: ' + ay2);
-//   print('EndX: ' + ex + ' EndY: ' + ey);
-// }
-
+  //For Debugging purposes
+  // function printPoints() {
+  //   print('MouseX: ' + mx + ' MouseY: ' + my);
+  //   print('Startx: ' + sx + ' Starty: ' + sy);
+  //   print('AnchorX1: ' + ax1 + ' AnchorY1: ' + ay1);
+  //   print('AnchorX2: ' + ax2 + ' AnchorY2: ' + ay2);
+  //   print('EndX: ' + ex + ' EndY: ' + ey);
+  // }
 }
